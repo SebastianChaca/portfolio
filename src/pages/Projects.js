ec902df868bd5bb27692869494ee74ad7f3c7e5b
SebastianChaca <sebastian.dalinger@gmail.com>
2021-02-08 01:47:52 -0300

    transition fixed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,17 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ProjectList from "../components/ProjectList";
 import { paginatedProjects } from "../content/paginate";
 import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 export default function Projects({ language }) {
   const [page, setPage] = useState(0);
-
+  const [fade, setFade] = useState(false);
+  const handleFade = () => {
+    setFade(true);
+    setTimeout(function () {
+      setFade(false);
+    }, 500);
+  };
+  useEffect(() => {
+    handleFade();
+  }, []);
   return (
     <div
       className={` ${paginatedProjects.length > 1 ? "pagination-on" : ""} ${
         paginatedProjects[page].length === 1
           ? "projects-container-one"
           : "projects-container"
-      } `}
+      } ${fade ? "transition-init" : ""} `}
     >
       <ProjectList projects={paginatedProjects[page]} language={language} />
       {paginatedProjects.length > 1 && (
