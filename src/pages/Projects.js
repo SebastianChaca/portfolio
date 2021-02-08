6268586073320565fd91871d4e2c1cf26c07e8e6
SebastianChaca <sebastian.dalinger@gmail.com>
2021-02-08 01:29:33 -0300

    useeffect removed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ProjectList from "../components/ProjectList";
 import { paginatedProjects } from "../content/paginate";
 import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
